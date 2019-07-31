807fbd56e4bdadd1581d2b1c4c4d2fe819c1ef75
Dajie Yang <dajie.yang@anu.edu.au>
2019-08-01 04:57:15 +1000

    Setup database connection

--- srpms/srpms/settings.py
+++ srpms/srpms/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'srpms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'srpms',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'srpms',
+        'PASSWORD': 'Srpms'
     }
 }
 
