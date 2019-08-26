34d6a2694f2181c403385900890429fb81de690f
Dajie Yang <dajie.yang@anu.edu.au>
2019-08-26 18:13:45 +1000

    1. Optimize development deployment settings to enable auto-reload on source change for django 2. Add nginx configuration for production (unfinished)

--- srpms/srpms/settings.py
+++ srpms/srpms/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files (user uploads)
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
