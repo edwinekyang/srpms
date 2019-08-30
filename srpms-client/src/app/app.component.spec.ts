6e11a2ac709057c02f30d0d3c33f96950b99f4a8
edwinyang <u6435883@anu.edu.au>
2019-08-30 14:57:36 +1000

    Modified project name "srpms" to "srpms-client"

--- srpms-client/src/app/app.component.spec.ts
+++ srpms-client/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'srpms'`, () => {
+  it(`should have as title 'srpms-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('srpms');
+    expect(app.title).toEqual('srpms-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('srpms app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('srpms-client app is running!');
   });
 });
