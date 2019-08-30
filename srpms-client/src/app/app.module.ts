bba34f23f655a9359180a7c0c975331741ca7e86
edwinyang <u6435883@anu.edu.au>
2019-08-30 11:41:35 +1000

    1. Initial commit for front-end with skeleton 2. Specify angular version in dockerfile 3. Fix static link in dev env

--- srpms-client/src/app/app.module.ts
+++ srpms-client/src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContractComponent } from './contract/contract.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MenuleftComponent } from './menuleft/menuleft.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContractComponent,
+    MenuleftComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
---
+++ srpms-client/src/app/contract/contract.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-contract',
+  templateUrl: './contract.component.html',
+  styleUrls: ['./contract.component.sass']
+})
+export class ContractComponent implements OnInit {
+
+  firstName = '';
+  lastName = '';
+  brews: Object;
+
+  // tslint:disable-next-line:variable-name
+  constructor(private _http: HttpService) { }
+
+  ngOnInit() {
+    this._http.getBeer().subscribe(data => {
+      this.brews = data;
+      console.log(this.brews);
+    });
+  }
+
+  setClasses() {
+    const myClasses = {
+      active: this.firstName === 'a',
+      notactive: this.firstName !== 'a',
+    };
+    return myClasses;
+  }
+}
