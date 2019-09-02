7df1db72d785f6f160a82aa20f07d03505f4b018
Dajie Yang <dajie.yang@anu.edu.au>
2019-09-02 20:12:40 +1000

    Handle signal in entrypoint for angular during development

--- srpms-client/start.sh
+++ srpms-client/start.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env sh
 
+pid=1
+
 cleanup() {
     echo "Exiting ..."
+
+    if [ $pid -ne 1 ]; then
+        kill -SIGTERM "-$pid"
+        wait "$pid"
+    fi
+
     exit
 }
-
-trap cleanup INT TERM
 
 set -e
 
 if [ "$DEBUG" == "True" ]; then
+    trap cleanup INT TERM
     npm install
-    ng build --watch --output-path /dist/srpms-client
+    ng build --watch --output-path /dist/srpms-client &
+    pid="${!}"
+    wait "$pid"
 else
     # Clean up and copy file to volume
     rm -rf /dist/*
