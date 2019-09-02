9b32ca617c9a8aea5fc71e4c6d97a66eeb184297
Euikyum Yang <u6435883@anu.edu.au>
2019-09-02 14:11:42 +1000

    1. Added Angular material ui 2. Changed component menuleft to sidenav 3. Added header component 4. Added master theme for the front-end

--- srpms-client/src/app/header/header.component.spec.ts
+++ srpms-client/src/app/header/header.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuleftComponent } from './menuleft.component';
+import { HeaderComponent } from './header.component';
 
-describe('MenuleftComponent', () => {
-  let component: MenuleftComponent;
-  let fixture: ComponentFixture<MenuleftComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuleftComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MenuleftComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
