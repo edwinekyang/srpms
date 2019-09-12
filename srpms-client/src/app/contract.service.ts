0e597a4f80fe0450f2fadc46a572ac5c3748b763
Euikyum Yang <u6435883@anu.edu.au>
2019-09-12 11:11:15 +1000

    HTTP header, JSONP request

--- srpms-client/src/app/contract.service.ts
+++ srpms-client/src/app/contract.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+export interface Course {
+  course_id: number;
+  course_number: string;
+  name: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +16,36 @@ export class ContractService {
 
   constructor(private http: HttpClient) { }
 
+  private API_URL = '/api/';
 
+  public httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  };
+
+  private static log(message: string) {
+    console.log(`Contract Service: ${message}`);
+  }
+
+  getCourses(): Observable<Course> {
+    return this.http.get<Course>(`${this.API_URL}/courses`, this.httpOptions)
+      .pipe(
+        catchError(this.handleError<Course>('getCourses'))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      ContractService.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
--- srpms-client/src/app/contract/contract.component.ts
+++ srpms-client/src/app/contract/contract.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse} from '@angular/common/http';
+import { ContractService, Course } from '../contract.service';
 
 @Component({
   selector: 'app-contract',
   templateUrl: './contract.component.html',
   styleUrls: ['./contract.component.scss']
 })
+
 export class ContractComponent implements OnInit {
+  errorMessage: string;
 
   // tslint:disable-next-line:variable-name
-  constructor() { }
+  constructor(
+    public course: Course,
+    public message: string,
+    private contractService: ContractService,
+    ) {
+      this.errorMessage = message;
+  }
 
   ngOnInit() {
+  }
+
+  showCourses() {
+    this.contractService.getCourses()
+      .subscribe(
+          (data: Course) => this.course = {
+        course_id: data.course_id,
+        course_number: data.course_number,
+        name: data.name},
+          error => {
+            if (error instanceof HttpErrorResponse) {
+              this.errorMessage = error.error.detail;
+            }
+          });
   }
 }
