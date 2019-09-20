828fe43859cd51e63e38bf2c5f620005fba4bcc1
Dajie Yang <dajie.yang@anu.edu.au>
2019-09-20 17:48:27 +1000

    APP Research_mgt: add custom serializer field to deal with approval

--- srpms/research_mgt/serializers.py
+++ srpms/research_mgt/serializers.py
@@ -1,7 +1,47 @@
+import json
+from datetime import datetime
 from rest_framework import serializers
 
 from accounts.models import SrpmsUser
 from . import models
+
+
+class ApprovalBooleanField(serializers.BooleanField):
+    """
+    Special field for retrieving approval status.
+
+    The database does not have a field 'is_approved', instead we check if
+    the 'approval_date' is empty to see if its approved.
+
+    This field can also used to write the DateTimeField, it'll set the
+    source field to datetime.now() if the post date is True.
+    """
+
+    def to_representation(self, value: bool) -> str:
+        return json.dumps(value)
+
+    def to_internal_value(self, data: str) -> datetime:
+        """
+        Return the value that would be used to update the DateTimeField. If
+        True, return the current date & time, otherwise return None.
+        """
+        is_approved = json.loads(data)
+
+        # Only allow boolean value
+        if not isinstance(is_approved, bool):
+            raise serializers.ValidationError("Should be a boolean value")
+
+        return datetime.now() if is_approved else None
+
+    def get_attribute(self, instance) -> bool:
+        """
+        Get attribute from the instance, the return would be passed to
+        `to_representation` function. Note that DRF have this behavior
+        that None return would not trigger `to_representation`, so we
+        need to explicitly set True/False here
+        """
+        attr = super(ApprovalBooleanField, self).get_attribute(instance)
+        return bool(attr)
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -23,15 +63,30 @@ class SpecialTopicSerializer(serializers.ModelSerializer):
 
 
 class ContractSerializer(serializers.ModelSerializer):
+    """
+    Contract serializer for all types of contract.
+
+    Regardless of the type of contract, they all using the parent class's (i.e. Contract)
+    primary key as their primary key, as such it's most convenient to have only one serializer
+    to take care of them all.
+
+    In this serializer, different types of contract would be serialized as a nested field. It
+    also support write for these nested field by overriding the `create` and `update` method.
+    """
+
     individual_project = IndividualProjectSerializer(source='individualproject',
                                                      required=False, allow_null=True)
     special_topics = SpecialTopicSerializer(source='specialtopics',
                                             required=False, allow_null=True)
 
+    convener_approval_date = serializers.ReadOnlyField()
+    is_convener_approved = ApprovalBooleanField(source='convener_approval_date', required=False)
+    create_date = serializers.ReadOnlyField()
+
     class Meta:
         model = models.Contract
         fields = ['id', 'year', 'semester', 'duration', 'resources', 'course', 'convener',
-                  'convener_approval_date', 'owner', 'create_date',
+                  'is_convener_approved', 'convener_approval_date', 'owner', 'create_date',
                   'individual_project', 'special_topics']
 
     def validate(self, attrs: dict):
@@ -86,9 +141,14 @@ class ContractSerializer(serializers.ModelSerializer):
 
 
 class SuperviseSerializer(serializers.ModelSerializer):
+    is_supervisor_approved = ApprovalBooleanField(source='supervisor_approval_date',
+                                                  required=False)
+    supervisor_approval_date = serializers.ReadOnlyField()
+
     class Meta:
         model = models.Supervise
-        fields = ['id', 'supervisor', 'is_formal', 'supervisor_approval_date', 'contract']
+        fields = ['id', 'contract', 'supervisor', 'is_formal', 'is_supervisor_approved',
+                  'supervisor_approval_date']
 
 
 class AssessmentTemplateSerializer(serializers.ModelSerializer):
@@ -98,10 +158,14 @@ class AssessmentTemplateSerializer(serializers.ModelSerializer):
 
 
 class AssessmentMethodSerializer(serializers.ModelSerializer):
+    is_examiner_approved = ApprovalBooleanField(source='examiner_approval_date',
+                                                required=False)
+    examiner_approval_date = serializers.ReadOnlyField()
+
     class Meta:
         model = models.AssessmentMethod
         fields = ['id', 'template', 'contract', 'additional_description', 'due', 'max_mark',
-                  'examiner', 'examiner_approval_date']
+                  'examiner', 'is_examiner_approved', 'examiner_approval_date']
 
 
 class UserContractSerializer(serializers.ModelSerializer):
