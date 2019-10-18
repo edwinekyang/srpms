4dc66b37bc235e602585f868b7d8c83c51c67c22
Dajie Yang <dajie.yang@anu.edu.au>
2019-10-18 13:33:58 +1100

    APP Research_mgt: correct user serializer's behavior - fix the issue that supervise and examine contract list is not distinct - add support for was_submitted field, so that was_submitted contract won't disappear from the list - remove un-necessary condition for filter of examine, supervise, and convene

--- srpms/research_mgt/serializers.py
+++ srpms/research_mgt/serializers.py
@@ -14,6 +14,7 @@ __email__ = "dajie.yang@anu.edu.au"
 
 from typing import Tuple
 from django.db import transaction
+from django.db.models.query import Q
 from rest_framework import serializers
 
 from accounts.models import SrpmsUser
@@ -60,9 +61,10 @@ class UserContractSerializer(serializers.ModelSerializer):
         Args:
             obj: the user that is being serialized currently
         """
-        return tuple(Contract.objects.filter(submit_date__isnull=False,
-                                             supervise__supervisor=obj)
-                     .values_list('pk', flat=True))
+        return tuple(Contract.objects.filter(
+                Q(submit_date__isnull=False) | Q(was_submitted=True),
+                supervise__supervisor=obj)
+                     .values_list('pk', flat=True).distinct())
 
     # noinspection PyMethodMayBeStatic
     def get_examine(self, obj: SrpmsUser) -> Tuple[int]:
@@ -73,8 +75,9 @@ class UserContractSerializer(serializers.ModelSerializer):
             obj: the user that is being serialized currently
         """
         return tuple(Contract.objects.filter(
-                assessment_examine__examine__examiner=obj,
-                supervise__supervisor_approval_date__isnull=False).values_list('pk', flat=True))
+                Q(supervise__supervisor_approval_date__isnull=False) | Q(was_submitted=True),
+                assessment_examine__examine__examiner=obj)
+                     .values_list('pk', flat=True).distinct())
 
     # noinspection PyMethodMayBeStatic
     def get_convene(self, obj: SrpmsUser) -> Tuple[int]:
@@ -88,10 +91,8 @@ class UserContractSerializer(serializers.ModelSerializer):
             return tuple(Contract.objects.all().values_list('pk', flat=True))
         elif obj.has_perm('research_mgt.can_convene'):
             return tuple(Contract.objects.filter(
-                    submit_date__isnull=False,
-                    supervise__supervisor_approval_date__isnull=False,
-                    assessment_examine__examiner_approval_date__isnull=False)
-                         .values_list('pk', flat=True))
+                    Q(submit_date__isnull=False) | Q(was_submitted=True))
+                         .values_list('pk', flat=True).distinct())
         else:
             return tuple()
 
